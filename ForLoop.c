fc317cf326203160cf4e5b3f51917745e40599b8
Fatih NALBANT <fatihnalbantiu@gmail.com>
2021-08-13 19:28:11 +0300

    Create ForLoop.c

---
+++ ForLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int numbers[] = {23, 4, 54, 67, 87, 54, 90, 94};
+    int i;
+    for(i = 0; i < 8; i++){
+        printf("%d\n", numbers[i]);
+    }
+    return 0;
+}
